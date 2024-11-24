78ca7b11b5b85e6b7ab6fd9ed2db7c32127c694b
uthso297 <uthso119464@gmail.com>
2024-11-24 19:17:21 +0600

    completed login part

--- Final_Project/BookShopManagementSystem.c
+++ Final_Project/BookShopManagementSystem.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// User Details
 typedef struct
 {
 
@@ -12,6 +13,7 @@ typedef struct
 User users[3];
 int userCount = 0;
 
+// Function to save user with file handling
 void saveUser()
 {
     FILE *fptr;
@@ -26,6 +28,7 @@ void saveUser()
     }
 }
 
+// Function to register user with file handling
 void registerUser()
 {
     printf("Registering user...\n");
@@ -51,38 +54,75 @@ void registerUser()
     }
 }
 
-void loginUser()
+// Function to login user
+int loginUser()
 {
     printf("Logging in user...\n");
+
+    char userName[30];
+    char userPass[20];
+
+    getchar();
+    printf("Enter Username: ");
+    fgets(userName, sizeof(userName), stdin);
+    userName[strcspn(userName, "\n")] = '\0';
+
+    printf("Enter Password: ");
+    fgets(userPass, sizeof(userPass), stdin);
+    userPass[strcspn(userPass, "\n")] = '\0';
+
+    for (int i = 0; i < userCount; i++)
+    {
+        if (strcmp(users[i].userName, userName) == 0 && strcmp(users[i].userPassword, userPass) == 0)
+        {
+            printf("Login successfully!\n");
+            return 1;
+        }
+    }
+
+    printf("Invalid username and password!\n");
+    return 0;
 }
 
+// Here is our main function
 int main()
 {
 
     int choice;
+
     do
     {
+
         printf("Enter your choice: \n");
         printf("1/Register\n");
         printf("2/Login\n");
         printf("3/Exit\n");
         scanf("%d", &choice);
         switch (choice)
+
         {
+
         case 1:
             // printf("This is registration\n");
             registerUser();
             break;
+
         case 2:
             // printf("This is login\n");
-            loginUser();
+            if (loginUser())
+            {
+                printf("Congrats!You are in login page");
+            }
             break;
+
         case 3:
             printf("Existing....\n");
             break;
+
         default:
             printf("Invalid Choice! Please try again\n");
         }
+
     } while (choice != 3);
 
     return 0;
